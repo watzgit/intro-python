65fb0a9e08147766448b7889dba9a55d44fd70ab
watzgit <152706281+watzgit@users.noreply.github.com>
2023-12-02 20:55:31 +0530

    input and returns values.py
    addition

--- input and returns values.py
+++ input and returns values.py
@@ -1,5 +1,5 @@
-print("hello")
-name=input("what is your name")
-print("hello" ,end="")
-print(name)
-
+#input
+x=input("x val")
+y=input("y val")
+z=int(x)+int(y) #Not z=x+y or z= int(x+y) both are concatination -joins both nunber
+print(z)
