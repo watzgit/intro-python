b6facb5dce4722b813a598803a513aca9d6a29a4
watzgit <152706281+watzgit@users.noreply.github.com>
2023-12-02 21:38:28 +0530

    input and returns values.py
    round in float

--- input and returns values.py
+++ input and returns values.py
@@ -1,5 +1,7 @@
-#input float
+#input float to round
 x=float(input("x val"))
-y=float(input("y val"))      #both take value and convert to decimal val
+y=float(input("y val"))      #both take value and convert to decimal val to round fig val
+                             #(2.3+2.2 )= 4.5
 
-print(x+y) 
+
+print(round(x+y))            #but in round (2.3 + 2.2) = 5
