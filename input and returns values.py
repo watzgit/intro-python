9315b0166e79feec8896923490f305838e2d2b23
watzgit <152706281+watzgit@users.noreply.github.com>
2023-12-02 21:35:04 +0530

    input and returns values.py
    float val

--- input and returns values.py
+++ input and returns values.py
@@ -1,5 +1,5 @@
-#input simplified
-x=int(input("x val"))
-y=int(input("y val"))      #both take value and convert to int 
+#input float
+x=float(input("x val"))
+y=float(input("y val"))      #both take value and convert to decimal val
 
-print(x+y) dont need z val bcz int assigned no concatination occurs
+print(x+y) 
