20ffeaaf206cd7b9698f1323cd5a081392ba3469
watzgit <152706281+watzgit@users.noreply.github.com>
2023-12-02 21:41:32 +0530

    input and returns values.py

--- input and returns values.py
+++ input and returns values.py
@@ -4,4 +4,4 @@ y=float(input("y val"))      #both take value and convert to decimal val to roun
                              #(2.3+2.2 )= 4.5
 
 
-print(round(x+y))            #but in round (2.3 + 2.2) = 5
+print(round(x+y))            #but in round (2.3 + 2.2) = 4
